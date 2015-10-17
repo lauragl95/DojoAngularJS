d489b0f6556f805b7d055aea167da40f3b88da2f
Laura <laurita5411@gmail.com>
2015-10-17 11:08:51 -0500

    Dojo Angular JS

---
+++ resources/js/services.js
@@ -0,0 +1,16 @@
+'use strict';
+angular.module('productos.services',[])
+	.factory('API',function ($http){
+		var base = 'http://spinnerbank-api-external.herokuapp.com';
+
+		return{
+
+		obtenerProductos:function(id){
+			return $http.get(base+ '/productos/'+id,{
+			method:'GET'
+			});
+		
+		}
+	};
+});
+
